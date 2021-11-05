1213b07a967a1fcb0b511a0fad4572e68403ca2f
aviash2507 <avinash2632@gmail.com>
2021-11-05 10:40:44 +0530

    First commit.

---
+++ basic_program.py
@@ -0,0 +1,8 @@
+print("Enter your Age ")
+age = int(input())
+if age > 18:
+    print("You can drive ")
+elif age == 18:
+    print("we will think about you")
+else:
+    print("you cannot drive until you are not 18")
